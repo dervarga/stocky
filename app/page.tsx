9b5c3fadf734706eec2fe83a6eb7ccfb2630f9c3
Daniel Varga <dantschi@gmail.com>
2025-03-07 15:00:22 +0100

    feat: add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { StockSearch } from '@/components/StockSearch'
 import { SymbolMatches } from '@/components/SymbolMatches'
+import { Suspense } from 'react'
 
 export default async function Home() {
   return (
     <div className='flex flex-grow flex-col items-center space-y-2 py-2'>
-      <StockSearch />
-      <SymbolMatches />
+      <Suspense fallback='Loading...'>
+        <StockSearch />
+        <SymbolMatches />
+      </Suspense>
     </div>
   )
 }
