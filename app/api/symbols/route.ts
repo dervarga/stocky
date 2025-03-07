97fe8eefe13d833ab6fd34ade0fe0e42133d1846
Daniel Varga <dantschi@gmail.com>
2025-03-07 14:57:38 +0100

    feat: implement initial symbol search

--- app/api/symbols/route.ts
+++ app/api/symbols/route.ts
@@ -1,19 +1,18 @@
-
-
 import { getSymbolsByKeyword } from '@/actions/stocks'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
-
   const { searchParams } = new URL(request.url)
   const searchTerm = searchParams.get('searchTerm')
 
   if (!searchTerm) {
-    return NextResponse.json({ message: 'No search term provided' }, { status: 400 })
+    return NextResponse.json(
+      { message: 'No search term provided' },
+      { status: 400 }
+    )
   }
 
   const matches = await getSymbolsByKeyword(searchTerm)
 
   return NextResponse.json(matches)
-
 }
